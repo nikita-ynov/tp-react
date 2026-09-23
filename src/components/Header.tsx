2e62782d1ebe51fa346fc342162d0a46380f5822
Shibler0 <rischmartin@laposte.net>
2026-09-23 15:57:58 +0200

    update header display and token cleaner

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,50 +2,41 @@ import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import type { RootState } from "../store/store";
 import { clearLoggedUser } from "../store/reducers/auth";
+import {clearFavorites} from "../store/reducers/favorites.ts";
 
 export default function Header() {
     const dispatch = useDispatch();
     const loggedUser = useSelector((state: RootState) => state.auth.loggedUser);
+    const loading = useSelector((state: RootState) => state.loading.value);
+
+    const logout = () => {
+        localStorage.removeItem("token");
+        dispatch(clearLoggedUser());
+        dispatch(clearFavorites());
+    };
 
     return (
         <header className="header">
             <nav className="header-nav container">
                 <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/users">Users</Link>
-                    </li>
-                    <li>
-                        <Link to="/favorites">Favorites</Link>
-                    </li>
-                    <li>
-                        <Link to="/profile">Profile</Link>
-                    </li>
-                    <li>
-                        <Link to="/blog">Blog</Link>
-                    </li>
+                    <li><Link to="/">Accueil</Link></li>
+                    <li><Link to="/users">Membres</Link></li>
+                    <li><Link to="/blog">Blog</Link></li>
                 </ul>
-                <ul>
-                    {
-                        !loggedUser ?
+
+                {!loading && (
+                    <ul>
+                        {loggedUser ? (
                             <>
-                                <li>
-                                    <Link to="/login">Login</Link>
-                                </li>
+                                <li><Link to="/profile">Mon profil</Link></li>
+                                <li><Link to="/favorites">Mes favoris</Link></li>
+                                <li><Link to="/" onClick={logout}>Déconnexion</Link></li>
                             </>
-                            :
-                            <>
-                                <li>
-                                    <Link to="/" onClick={() => {
-                                        localStorage.removeItem("token");
-                                        dispatch(clearLoggedUser());
-                                    }}>Log out</Link>
-                                </li>
-                            </>
-                    }
-                </ul>
+                        ) : (
+                            <li><Link to="/login">Connexion</Link></li>
+                        )}
+                    </ul>
+                )}
             </nav>
         </header>
     );
