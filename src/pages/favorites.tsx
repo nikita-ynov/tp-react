351905f8f03c5f0e0d6f4d85ddac5e084e1b573b
Shibler0 <rischmartin@laposte.net>
2026-09-23 11:43:20 +0200

    fix bad import

--- src/pages/favorites.tsx
+++ src/pages/favorites.tsx
@@ -3,37 +3,42 @@ import {Link} from "react-router-dom";
 import FavoriteButton from "../components/FavoriteButton.tsx";
 import type {RootState} from "../store/store.ts";
 
-const Favorites = () => {
+function Favorites() {
     const favorites = useSelector(
         (state: RootState) => state.favorites.recipes
     );
 
-    if (favorites.length === 0) {
-        return <p>Vous n’avez aucune recette favorite.</p>;
-    }
-
     return (
-        <main>
-            <h1>Mes favoris</h1>
+        <>
+            <h1>Mes recettes favorites</h1>
+
+            {favorites.length === 0 && (
+                <p>Aucune recette favorite.</p>
+            )}
 
             <div className="recipes-grid">
-                {favorites.map(recipe => (
-                    <article className="recipe-card" key={recipe.id}>
-                        <Link to={`/recipedetails/${recipe.id}`}>
-                            <img src={recipe.image} alt={recipe.name}/>
-                            <h2>{recipe.name}</h2>
+                {favorites.map((recipe) => (
+                    <div key={recipe.id}>
+                        <Link
+                            className="recipe-link"
+                            to={`/recipedetails/${recipe.id}`}
+                        >
+                            <div className="recipe-card">
+                                <h2>{recipe.name}</h2>
 
-                            <p>
-                                Préparation : {recipe.prepTimeMinutes} min
-                            </p>
+                                <img
+                                    src={recipe.image}
+                                    alt={recipe.name}
+                                />
+                            </div>
                         </Link>
 
                         <FavoriteButton recipe={recipe}/>
-                    </article>
+                    </div>
                 ))}
             </div>
-        </main>
-    );
-};
+        </>
+    )
+}
 
 export default Favorites;
